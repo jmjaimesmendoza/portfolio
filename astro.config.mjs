9f59776cf7a9a4558cbac2fd0b27dc7c181bd2cb
jmjaimesmendoza <jmjaimesmendoza@gmail.com>
2022-09-13 19:35:34 -0600

    added partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
-
 import mdx from "@astrojs/mdx";
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte(), mdx()]
+  integrations: [tailwind(), svelte(), mdx(), partytown()]
 });
